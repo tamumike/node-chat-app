7e1676faf2cf6290882ab56fa0372ed7aa88d8cd
tamumike <30604521+tamumike@users.noreply.github.com>
2018-07-17 13:55:01 -0500

    add new message and create message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit('newEmail', {
+  //   from: 'tamulinden@gmail.com',
+  //   text: 'Hey, what\'s going on?',
+  //   createdAt: 123
+  // });
+
+  socket.emit('newMessage', {
+    from: 'Augusta',
+    text: 'Can I have 2 dinners?',
+    createdAt: 400
+  });
+
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('createEmail', newEmail);
+  // });
+
+  socket.on('createMessage', (msg) => {
+    console.log('createMessage', msg);
+  });
+
   socket.on('disconnect', (reason) => {
     console.log('User was disconnected');
   });
@@ -22,3 +42,5 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log(`Started server on ${port}`);
 });
+
+// newMessage, createMessage (from, text, createdAt)
